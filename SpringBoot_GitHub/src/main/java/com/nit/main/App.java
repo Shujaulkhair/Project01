88563cf8484f2b658930f17b8f2affc9db40c712
Shuja <Shuja@DESKTOP-667F718>
2024-11-12 16:47:34 +0530

    Adding two number -- TL

--- SpringBoot_GitHub/src/main/java/com/nit/main/App.java
+++ SpringBoot_GitHub/src/main/java/com/nit/main/App.java
@@ -6,5 +6,9 @@ package com.nit.main;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a = 5;
+        int b = 9;
+        int c = a + b;
+        System.out.println(c);
     }
 }
